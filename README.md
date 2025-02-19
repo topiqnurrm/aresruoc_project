2079ecf36c8eba4ebf52971141cda308384f2807
toopique <sossmeed@gmail.com>
2025-02-19 18:57:12 +0700

    Menghapus semua file kecuali test-med-app

--- README.md
+++
@@ -1,1 +0,0 @@
-frontend_capstone_starter_code
--- config.js
+++
@@ -1,6 +0,0 @@
-// export const API_URL = window.location.hostname === "localhost" ? "https://hasanmathju-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai" : "add your theia server side url";
-export const API_URL = 'http://localhost:8181'
-console.log(
-    "API_URL :",
-    API_URL
-);
