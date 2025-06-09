b1e5d4b6905a877a4296318a55894a8c2f342c7a
toopique <sossmeed@gmail.com>
2025-06-09 12:44:14 +0700

    untuk memenuhi course iOS App Capstone, Peer-graded Assignment Little Lemon Food Ordering App - Introduction to iOS Mobile Application Development

---
+++ order-Little-lemon/LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,16 @@
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
